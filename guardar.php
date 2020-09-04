0bcf005520d357008632856b604e1a5d94bb5ca1
Edgar Mendoza <eomendoza1@utpl.edu.ec>
2020-09-04 13:17:17 -0500

    Corrección de bugs y vulnerabilidades Edgar Mendoza

--- guardar.php
+++ guardar.php
@@ -71,9 +71,9 @@
 	}
 
 	function verificar_resultado( $resultado ){
-		if( !$resultado )	$informacion["respuesta"] = "ERROR";
-		else 	$informacion["respuesta"] = "BIEN";
-		echo json_encode( $informacion );
+		if( !$resultado ){	$informacion["respuesta"] = "ERROR";}
+		else 	{$informacion["respuesta"] = "BIEN";
+		echo json_encode( $informacion );}
 	}
 
 	function cerrar( $conexion ){
