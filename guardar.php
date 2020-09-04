08fe383aaa28d6c9e6d379a74da2783dff9dfcda
Edgar Mendoza <eomendoza1@utpl.edu.ec>
2020-09-04 13:32:52 -0500

    Corrección de code smells Edgar Mendoza

--- guardar.php
+++ guardar.php
@@ -73,7 +73,8 @@
 	function verificar_resultado( $resultado ){
 		if( !$resultado ){	$informacion["respuesta"] = "ERROR";}
 		else 	{$informacion["respuesta"] = "BIEN";
-		echo json_encode( $informacion );}
+		echo json_encode( $informacion );
+		}
 	}
 
 	function cerrar( $conexion ){
